1e3c6dd96d535359d1a3d06b4da07f2731522a24
Vovka67 <114399957+Vovka67@users.noreply.github.com>
2023-02-06 19:12:40 +0500

    Add files via upload

---
+++ 246.js
@@ -0,0 +1,22 @@
+// #1
+// elem1.addEventListener('dblclick', function() {
+// 	console.log('Привет ');
+// });
+
+// #2
+// elem1.addEventListener('mouseover', function() {
+// 	console.log('hello!');
+// });
+
+// #3
+// elem1.addEventListener('mouseout', function() {
+// 	console.log('home');
+// });
+
+// #4
+// elem1.addEventListener('mouseover', function() {
+// 	console.log('Пока ');
+// });
+// elem1.addEventListener('mouseout', function() {
+// 	console.log('Снег ');
+// });
